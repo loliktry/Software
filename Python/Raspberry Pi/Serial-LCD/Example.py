ab4ea25e855b556f51992976405391c62eadfbf1
Daniel Kampert <DanielKampert@kampis-elektroecke,de>
2013-11-09 20:47:10 +0100

    Adding Example, edit Readme

---
+++ Python/Raspberry Pi/Serial-LCD/Example.py
@@ -0,0 +1,24 @@
+from LCD_Serial import LCD_Serial
+import time
+
+# Initialize the Module for an LCD with 20 Columns per Line
+LCD = LCD_Serial("/dev/ttyAMA0", 9600, 20)
+
+# Delete Display
+LCD.Clr()
+
+# Outputstring
+String = "Test"
+
+while True:
+	for I in range(1, 21 - len(String)):
+		LCD.Position(3, I)
+		LCD.Write(String)
+		time.sleep(1)
+		LCD.Clr()
+	
+	for I in range(21 - len(String), 0, -1):
+		LCD.Position(3, I)
+		LCD.Write(String)
+		time.sleep(1)
+		LCD.Clr()
